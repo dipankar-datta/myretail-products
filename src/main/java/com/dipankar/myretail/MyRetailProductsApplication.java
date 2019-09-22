cf51583f53190f9bfd7b18398babf82ecad94530
dipankar-datta <dattapro5@gmail.com>
2019-09-22 20:38:21 +0530

    Initial commit.

---
+++ src/main/java/com/dipankar/myretail/MyRetailProductsApplication.java
@@ -0,0 +1,13 @@
+package com.dipankar.myretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRetailProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRetailProductsApplication.class, args);
+	}
+
+}
