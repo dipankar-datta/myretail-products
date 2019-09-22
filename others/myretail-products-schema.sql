d4dbc00d305d7303b5ca3b6106749af1947f48d6
dipankar-datta <dattapro5@gmail.com>
2019-09-22 21:59:30 +0530

    Added DB schema with data file.

--- others/myretail-products-schema.sql
+++ others/myretail-products-schema.sql
@@ -13,12 +13,10 @@
 
 
 -- Dumping database structure for myretail_products
-DROP DATABASE IF EXISTS `myretail_products`;
 CREATE DATABASE IF NOT EXISTS `myretail_products` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `myretail_products`;
 
 -- Dumping structure for table myretail_products.categories
-DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
@@ -26,12 +24,11 @@ CREATE TABLE IF NOT EXISTS `categories` (
   PRIMARY KEY (`id`),
   KEY `id` (`id`),
   KEY `code` (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table myretail_products.category_subcategory_map
-DROP TABLE IF EXISTS `category_subcategory_map`;
 CREATE TABLE IF NOT EXISTS `category_subcategory_map` (
   `category` int(11) DEFAULT NULL,
   `subcategory` int(11) DEFAULT NULL,
@@ -42,10 +39,10 @@ CREATE TABLE IF NOT EXISTS `category_subcategory_map` (
 -- Data exporting was unselected.
 
 -- Dumping structure for table myretail_products.products
-DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` varchar(50) DEFAULT NULL,
+  `public_id` varchar(200) DEFAULT NULL,
   `segment` int(11) DEFAULT NULL,
   `category` int(11) DEFAULT NULL,
   `subcategory` int(11) DEFAULT NULL,
@@ -58,28 +55,49 @@ CREATE TABLE IF NOT EXISTS `products` (
   KEY `id` (`id`),
   KEY `FK_proucts_categories` (`category`),
   KEY `FK_proucts_subcategories` (`subcategory`),
-  KEY `FK_products_segments` (`segment`),
-  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`segment`) REFERENCES `segments` (`id`),
-  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`category`) REFERENCES `categories` (`id`),
-  CONSTRAINT `products_ibfk_3` FOREIGN KEY (`subcategory`) REFERENCES `subcategories` (`id`)
+  KEY `FK_products_segments` (`segment`)
+) ENGINE=InnoDB AUTO_INCREMENT=9240 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table myretail_products.product_stock_map
+CREATE TABLE IF NOT EXISTS `product_stock_map` (
+  `product` int(11) DEFAULT NULL,
+  `unit` int(11) DEFAULT NULL,
+  KEY `FK__proucts_map` (`product`),
+  KEY `FK__product_units` (`unit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table myretail_products.segments
-DROP TABLE IF EXISTS `segments`;
 CREATE TABLE IF NOT EXISTS `segments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`),
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table myretail_products.stock_details
+CREATE TABLE IF NOT EXISTS `stock_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product` int(11) NOT NULL DEFAULT '0',
+  `unit` varchar(50) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` double NOT NULL DEFAULT '0',
+  `discount_percentage` float DEFAULT NULL,
+  `discount_price` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`),
+  KEY `FK__proucts` (`product`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table myretail_products.subcategories
-DROP TABLE IF EXISTS `subcategories`;
 CREATE TABLE IF NOT EXISTS `subcategories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
@@ -87,7 +105,7 @@ CREATE TABLE IF NOT EXISTS `subcategories` (
   PRIMARY KEY (`id`),
   KEY `id` (`id`),
   KEY `code` (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- Data exporting was unselected.
 
